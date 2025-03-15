085bb5fd6bd2e35f2d1c172e52d06f9ca0b163c4
CodeMeNawh <89380204+CodeMeNawh@users.noreply.github.com>
2025-03-15 10:58:58 +0100

    Mikolaj ExamJanuary

---
+++ June2023_technical/Repositories/BookRepository.cs
@@ -0,0 +1,42 @@
+﻿using January2024_technical.Data;
+using January2024_technical.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace January2024_technical.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookContext _context;
+
+        public BookRepository(BookContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Book book)
+        {
+            await _context.Books.AddAsync(book);
+        }
+
+        public async Task DeleteAsync(Book book)
+        {
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Book>> GetAllAsync()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        public async Task<Book?> GetByIdAsync(int id)
+        {
+            return await _context.Books.FindAsync(id);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
